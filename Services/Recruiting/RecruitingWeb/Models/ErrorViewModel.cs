32a3b1da55375f5541eb9e890f38f9ab6b3342ec
yangql15 <yangql15@bu.edu>
2023-03-14 14:40:33 -0400

    update

---
+++ Services/Recruiting/RecruitingWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RecruitingWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
